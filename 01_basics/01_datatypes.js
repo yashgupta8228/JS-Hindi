a21af96bb6c95dbc600588362f915d4cce005ebc
yashgupta8228 <70104926+yashgupta8228@users.noreply.github.com>
2024-03-28 19:32:17 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"  // treat alljs code as newer version 
+
+console.log(3+3)
+console.log("Yash")
+
+let accountNamename= "Yash"
+let accounID = 515165
+
+console.log(typeof "Yash");
+console.log(typeof accounID);
