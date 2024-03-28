e8526021bf326e44a408cb38a9c682301dea6b85
yashgupta8228 <70104926+yashgupta8228@users.noreply.github.com>
2024-03-28 13:12:34 +0000

    variable config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accounID = 415111
+let accountName = "node"
+let acoountEmail = "yash@gmail.com"
+let acoountpass = "1235445"
+let accountState;
+
+console.table([accounID, accountName, acoountEmail, acoountpass, accountState])
