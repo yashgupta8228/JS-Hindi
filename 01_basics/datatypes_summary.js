e31de3294ea0af33682a0703d8c17f4e20aa8bea
yashgupta8228 <70104926+yashgupta8228@users.noreply.github.com>
2024-04-08 14:30:32 +0000

    Datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,32 @@
+/*primitive 
+
+There are 7 types of datatypes:- String, Number , boolean, null, undegined, Symbol, BigInt
+*/ 
+
+score = 100
+
+isLogged = false
+
+const temp = null
+
+const id = Symbol(1234)
+
+console.log(typeof isLogged);
+console.log(typeof temp);
+console.log(typeof score);
+console.log(typeof id);
+
+/*
+# Non primitive 
+
+Array, Function, Objects
+
+**for all non primitive time datatype is object .
+*/
+
+const heros = ["Ironman" , "Hulk", "Caption America"]
+
+let myObj = {
+    name: "Yash",
+    age: 20
+}
