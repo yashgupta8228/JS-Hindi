bca17582cfc549a382401051ed21875138deb1a0
yashgupta8228 <70104926+yashgupta8228@users.noreply.github.com>
2024-05-01 19:44:53 +0000

    Array Ans Objects

---
+++ 02_basics/04_object.js
@@ -0,0 +1,28 @@
+// const tinderUser = new Object()
+const tinderUser={}
+
+tinderUser.id = 123
+tinderUser.name = "Yash"
+tinderUser.PhoneNo = 7985453807
+
+// console.log(tinderUser);
+
+const regularUser={
+    email: "Yash@gmail.com",
+    fullname:{
+        userFullname:{
+            firstName:"Yash",
+            lastName:"Gupta"
+        }
+    }
+}
+// console.log(regularUser.fullname.userFullname);
+
+// how to combine 2 objects
+
+const obj1={1:"a", 2:"b"}
+const obj2={3:"c", 4:"d"}
+
+// const obj3 = {obj1, obj2}
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
