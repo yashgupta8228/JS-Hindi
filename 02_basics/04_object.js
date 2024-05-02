f8828ea7ee62711fbb0bf48e4b2b19afc578cc4b
yashgupta8228 <70104926+yashgupta8228@users.noreply.github.com>
2024-05-02 10:11:59 +0000

    json

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -4,6 +4,7 @@ const tinderUser={}
 tinderUser.id = 123
 tinderUser.name = "Yash"
 tinderUser.PhoneNo = 7985453807
+tinderUser.isLoggedin = false
 
 // console.log(tinderUser);
 
@@ -24,5 +25,28 @@ const obj1={1:"a", 2:"b"}
 const obj2={3:"c", 4:"d"}
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1, obj2)
-console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2)
+// const obj3 = Object.assign(target,source)
+const obj3 = {...obj1, ...obj2} //spread method 
+// console.log(obj3);
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(Object.values(tinderUser));
+
+// De-Structuring Object
+
+const cource = {
+    courceInstructor: "Hitesh",
+    courceName: "JS",
+    Student:"Yash"
+}
+
+// const {courceInstructor: instuctor} = cource
+const {courceName: Name}=cource 
+const {Student} = cource
+
+console.log(cource.courceInstructor);
+console.log(Name);
+console.log(Student);
